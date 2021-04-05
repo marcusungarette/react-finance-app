e08fde6c6411c98f10c7e5c74e298206a43b7634
marcusungarette <marcusungarette@gmail.com>
2021-04-05 15:14:57 -0300

    Added - ModalOnClickFunction passed with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransaction] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransaction(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransaction(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
